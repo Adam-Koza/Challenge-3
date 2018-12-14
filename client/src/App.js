8ded0dcc0b7dad0de7c273a871d2d7ad23d9b479
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-12-13 21:44:30 -0500

    Add token contract to example.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import TokenContract from "./contracts/Token.json";
+//import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 import truffleContract from "truffle-contract";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { balance: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -17,7 +18,7 @@ class App extends Component {
       const accounts = await web3.eth.getAccounts();
 
       // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
+      const Contract = truffleContract(TokenContract);
       Contract.setProvider(web3.currentProvider);
       const instance = await Contract.deployed();
 
@@ -36,14 +37,14 @@ class App extends Component {
   runExample = async () => {
     const { accounts, contract } = this.state;
 
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
+    // Get balance of user account.
+    // await contract.balanceOf(accounts[0]);//, { from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.get();
+    // Get the balance from the contract to prove it worked.
+    const response = await contract.balanceOf(accounts[0]);
 
     // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
+    this.setState({ balance: response.toNumber() });
   };
 
   render() {
@@ -57,12 +58,12 @@ class App extends Component {
         <h2>Smart Contract Example</h2>
         <p>
           If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
+          a Balance of 10000 (by default).
         </p>
         <p>
           Try changing the value stored on <strong>line 40</strong> of App.js.
         </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>Your balance is: {this.state.balance}</div>
       </div>
     );
   }
