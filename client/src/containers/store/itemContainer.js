2acd2cf2c6cc0c3c79c480b14c1e0e58dfac3e9c
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-12-15 11:30:22 -0500

    Add dummy store items.

--- client/src/containers/store/itemContainer.js
+++ client/src/containers/store/itemContainer.js
@@ -21,39 +21,39 @@ export default class ItemContainer extends Component {
                 date: "July 14"
             }, {
                 id: 2,
-                name: "Rock The Park",
-                handle: "London, ON",
-                body: "../../../images/rtp.jpg",
-                price: 2000,
-                date: "July 14"
+                name: "School of Rock",
+                handle: "Toronto, ON",
+                body: "../../../images/sor.jpg",
+                price: 1000,
+                date: "Dec 22"
             }, {
                 id: 3,
-                name: "Rock The Park",
-                handle: "London, ON",
-                body: "../../../images/rtp.jpg",
-                price: 2000,
-                date: "July 14"
+                name: "Silverstein",
+                handle: "Toronto, ON",
+                body: "../../../images/sil.jpeg",
+                price: 1500,
+                date: "Dec 15"
             }, {
                 id: 4,
-                name: "Rock The Park",
-                handle: "London, ON",
-                body: "../../../images/rtp.jpg",
-                price: 2000,
-                date: "July 14"
+                name: "Krimer",
+                handle: "Toronto, ON",
+                body: "../../../images/kri.jpeg",
+                price: 1700,
+                date: "Dec 15"
             }, {
                 id: 5,
-                name: "Rock The Park",
-                handle: "London, ON",
-                body: "../../../images/rtp.jpg",
-                price: 2000,
-                date: "July 14"
+                name: "Med. French Fries",
+                handle: "Canada",
+                body: "../../../images/fri.jpeg",
+                price: 100,
+                date: "Coupon"
             }, {
                 id: 6,
-                name: "Rock The Park",
-                handle: "London, ON",
-                body: "../../../images/rtp.jpg",
-                price: 2000,
-                date: "July 14"
+                name: "Blue Jays",
+                handle: "Toronto, ON",
+                body: "../../../images/blu.jpeg",
+                price: 600,
+                date: "2019"
             }, {
                 id: 7,
                 name: "Rock The Park",
