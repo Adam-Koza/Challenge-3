bb39e1e923c72b887f1b2c5bbeac3392a70efee6
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-12-15 11:46:08 -0500

    Mod store item styling.

--- client/src/components/store/item.js
+++ client/src/components/store/item.js
@@ -7,7 +7,7 @@ import ItemTile from "./itemTile";
 const Item = props => {
 	return (
 		<ItemTile>
-			<div>
+			<div className="parent">
 				<div className="item-container">
 					<h4 className="name">{props.name}</h4>
 					<h4 className="handle">{props.handle} - {props.date}</h4>
