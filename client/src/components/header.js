4a23adc0670d01cc59ea5ec1c827c628e8a9d450
yanesh <yaneshcode@gmail.com>
2018-12-15 10:10:11 -0500

    Revert "big buttons"
    This reverts commit 34c839710bdeacc5f04e6c84b2d99db19f06a909.

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-// CSS
 import "./header.css";
 
 const Header = props => {
   return (
     <div className="header-bar">
-      <img src="../../public/images/carecoin_icon.png"/>{props.title}
+      {props.title}
+      <br/>{props.aprop}
     </div>
   )
 }
--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -9,27 +9,29 @@ const HomePage = props => {
   return (
     <div class="home-container">
       <div class="home-title">
-        <img src="../../public/images/carecoin.png"/>
-        <h1>Welcome to <b>CareCoin</b> - a decentralized donation app!</h1>
+        <h1>Welcome to CareCoin - decentralized donation app!</h1>
       </div>
       <br />
-      <div class="home-items col-7">
+      <div class="home-items">
           <br />
           <p>
+            <h3>Donate and receive CareCoin tokens:</h3>
             <LinkContainer to="/donation/">
-              <Button bsStyle="success" bsSize="large" block><h1>Donate and receive CareCoin tokens</h1></Button>
+              <Button bsStyle="success" bsSize="large">Donation</Button>
             </LinkContainer>
           </p>
           <br />
           <p>
+            <h3>Redeem CareCoin tokens:</h3>
             <LinkContainer to="/store/">
-              <Button bsStyle="primary" bsSize="large" block><h1>Redeem CareCoin tokens at the Store</h1></Button>
+              <Button bsStyle="primary" bsSize="large">Store</Button>
             </LinkContainer>
           </p>
           <br />
           <p>
+            <h3>View your profile:</h3>
             <LinkContainer to="/profile/">
-              <Button bsStyle="secondary" bsSize="large" block><h1>View your CareCoin profile</h1></Button>
+              <Button bsStyle="info" bsSize="large">Profile</Button>
             </LinkContainer>
           </p>
 
