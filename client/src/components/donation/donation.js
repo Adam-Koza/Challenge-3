46788cf4fbe163ec9bc780aca9015aadf8d1d729
yanesh <yaneshcode@gmail.com>
2018-12-15 08:14:19 -0500

    footer msg

--- client/src/components/donation/donation.js
+++ client/src/components/donation/donation.js
@@ -13,7 +13,7 @@ export default class Donation extends Component {
     return (
       <div class="donation-container">
         <h2>Donate and receive CareCoins!</h2>
-        <p>Donate items and once verified you will receive CareCoin tokens. Below are items that we are currently accepting and the amount of tokens that you will receive. Thanks for helping the community.</p>
+        <p>Donate items and once verified you will receive CareCoin tokens. Below are items that we are currently accepting and the amount of tokens that you will receive. Thanks for helping the community!</p>
         <div className="donation-item-list">
           <DonationItemContainer donate={this.props.donate} />
         </div>
--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -6,8 +6,8 @@ const Footer = props => {
   return (
     <div className="footer">
         <div className='footer-item footer-one'>Yanesh Prithipaul</div>
-        <div className='footer-item footer-two'>Powered by Etherscan.io APIs</div>
-        <div className='footer-item footer-three'>yaneshcode@gmail.com</div>
+        <div className='footer-item footer-two'>Powered by Truffle, Ganache, React, Bootstrap, Web3js</div>
+        <div className='footer-item footer-three'>Adam Koza</div>
     </div>
   )
 }
