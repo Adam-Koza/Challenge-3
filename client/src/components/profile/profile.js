059172405f10d7a7ff7a582321014f65bb4b6c2b
yanesh <yaneshcode@gmail.com>
2018-12-15 06:42:42 -0500

    profile mock data

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -5,8 +5,38 @@ import "./profile.css";
 
 const Profile = props => {
   return (
-    <div class="profile-container">
+    <div className="profile-container">
       <h2>View your awesome profile!</h2>
+      <p>Review your CareCoin token balance, past donation history, and redeemed items.</p>
+      <hr />
+      <br />
+      <p>
+        <hr />
+        <h3>Total balance:</h3>
+        <span>420 CareCoins</span>
+        <hr />
+        <h3>Donation history:</h3>
+        <ul>
+          <li>
+            <span>Men's Clothes</span>
+          </li>
+          <li>
+            <span>Canned Food</span>
+          </li>
+          <li>
+            <span>Kid's Clothes</span>
+          </li>
+        </ul>
+        <hr />
+          <h3>Redeemed Items:</h3>
+          <ul>
+            <li>
+              <span>Some awesome concert</span>
+            </li>
+          </ul>
+          <hr />
+      </p>
+
     </div>
   )
 }
