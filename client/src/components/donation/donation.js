6aa6cd3e7d54943d5defdd69762b0885624009d9
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-12-15 06:53:36 -0500

    Stable.

--- client/src/components/donation/donation.js
+++ client/src/components/donation/donation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import DonationItemContainer from '../../containers/donation/donationContainer';
 
 // CSS
@@ -10,13 +10,14 @@ export default class Donation extends Component {
   }
 
   render() {
-  return (
-    <div class="donation-container">
-      <h2>Donate and receive CareCoins!</h2>
-      <p>Donate items and once verified you will receive CareCoin tokens. Below are items that we are currently accepting and the amount of tokens that you will receive. Thanks for helping the community.</p>
-      <div className="donation-item-list">
-        <DonationItemContainer donate={this.props.donate} />
+    return (
+      <div class="donation-container">
+        <h2>Donate and receive CareCoins!</h2>
+        <p>Donate items and once verified you will receive CareCoin tokens. Below are items that we are currently accepting and the amount of tokens that you will receive. Thanks for helping the community.</p>
+        <div className="donation-item-list">
+          <DonationItemContainer donate={this.props.donate} />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
