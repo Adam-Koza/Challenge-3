25bd2d21409a67f8b784193ccb6900ec0d87ffd0
yanesh <yaneshcode@gmail.com>
2018-12-15 07:32:14 -0500

    initial footer placement

--- client/src/App.js
+++ client/src/App.js
@@ -101,18 +101,7 @@ class App extends Component {
             <NavHeader />
             <Routes balance={this.state.balance} buyItem={this.buyItem} donate={this.donate} />
 
-
-            {/*<h1>Good to Go!</h1>
-            <p>Your Truffle Box is installed and ready.</p>
-            <h2>Smart Contract Example</h2>
-            <p>
-            If your contracts compiled and migrated successfully, below will show
-            a Balance of 10000 (by default).
-            </p>
-            <p>
-            Try changing the value stored on <strong>line 40</strong> of App.js.
-            </p>
-            <div>Your balance is: {this.state.balance}</div>*/}
+            <NavHeader />
           </div>
         </div>
       </Router>
