f1b97afca160a11641a2f630414c10e7a8772ee0
yanesh <yaneshcode@gmail.com>
2018-12-15 10:26:51 -0500

    banner image

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+// CSS
 import "./header.css";
+
+// url strings
+const ICON_URL = "../../images/carecoin_icon.png";
 
 const Header = props => {
   return (
     <div className="header-bar">
-      <img className="header-img" src="../../images/carecoin_icon.png"/>
+      <img className="header-img" src={ICON_URL}/>
        &nbsp;{props.title}
     </div>
   )
--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -5,33 +5,34 @@ import { Button } from 'react-bootstrap';
 // CSS
 import "./homePage.css";
 
+// url strings
+const IMAGE_URL = "../../images/carecoin.png";
+
 const HomePage = props => {
   return (
     <div class="home-container">
+      <img src={IMAGE_URL} className="banner-image"/>
       <div class="home-title">
         <h1>Welcome to CareCoin - decentralized donation app!</h1>
       </div>
       <br />
-      <div class="home-items">
+      <div class="home-items col-7">
           <br />
           <p>
-            <h3>Donate and receive CareCoin tokens:</h3>
             <LinkContainer to="/donation/">
-              <Button bsStyle="success" bsSize="large">Donation</Button>
+              <Button bsStyle="success" bsSize="large" block><h1>Donate and receive CareCoin tokens</h1></Button>
             </LinkContainer>
           </p>
           <br />
           <p>
-            <h3>Redeem CareCoin tokens:</h3>
             <LinkContainer to="/store/">
-              <Button bsStyle="primary" bsSize="large">Store</Button>
+              <Button bsStyle="primary" bsSize="large" block><h1>Redeem CareCoin tokens in our Store</h1></Button>
             </LinkContainer>
           </p>
           <br />
           <p>
-            <h3>View your profile:</h3>
             <LinkContainer to="/profile/">
-              <Button bsStyle="info" bsSize="large">Profile</Button>
+              <Button bsStyle="info" bsSize="large" block><h1>View your CoinCare profile</h1></Button>
             </LinkContainer>
           </p>
 
